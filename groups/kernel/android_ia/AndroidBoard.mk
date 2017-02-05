8f85fa09da9bc3c539c574b16a6171de5ce76607
Kalyan Kondapally <kalyan.kondapally@intel.com>
2017-02-04 17:36:38 -0800

    Make all modules inbuilt.
    This is the preferred way to do things on Android.
    https://source.android.com/devices/tech/config/kernel.html
    
    Jira: AIA-121
    Test: Mixin update succeeds.
    
    Signed-off-by: Kalyan Kondapally <kalyan.kondapally@intel.com>

--- groups/kernel/android_ia/AndroidBoard.mk
+++ groups/kernel/android_ia/AndroidBoard.mk
@@ -7,11 +7,9 @@ TARGET_KERNEL_SRC ?= kernel/android_ia
 {{#x86_64}}
 TARGET_KERNEL_ARCH := x86_64
 TARGET_KERNEL_CONFIG ?= kernel_64_defconfig
-ADDITIONAL_DEFAULT_PROPERTIES += ro.boot.moduleslocation=/system/lib64/modules
 {{/x86_64}}
 
 {{^x86_64}}
-ADDITIONAL_DEFAULT_PROPERTIES += ro.boot.moduleslocation=/system/lib/modules
 $(error 32bit Kernel builds are not supported.)
 {{/x86_64}}
 
@@ -22,7 +20,6 @@ KERNEL_NAME := bzImage
 # Set the output for the kernel build products.
 KERNEL_OUT := $(abspath $(TARGET_OUT_INTERMEDIATES)/kernel)
 KERNEL_BIN := $(KERNEL_OUT)/arch/$(TARGET_KERNEL_ARCH)/boot/$(KERNEL_NAME)
-KERNEL_MODULES_INSTALL := $(TARGET_OUT)/lib/modules
 
 KERNELRELEASE = $(shell cat $(KERNEL_OUT)/include/config/kernel.release)
 
@@ -44,38 +41,8 @@ $(KERNEL_CONFIG): $(KERNEL_CONFIG_FILE)
 
 # Produces the actual kernel image!
 $(PRODUCT_OUT)/kernel: $(KERNEL_CONFIG) | $(ACP)
-	$(build_kernel) $(KERNEL_NAME) modules
+	$(build_kernel) $(KERNEL_NAME)
 	$(hide) $(ACP) -fp $(KERNEL_BIN) $@
-
-ALL_EXTRA_MODULES := $(patsubst %,$(TARGET_OUT_INTERMEDIATES)/kmodule/%,$(TARGET_EXTRA_KERNEL_MODULES))
-$(ALL_EXTRA_MODULES): $(TARGET_OUT_INTERMEDIATES)/kmodule/%: $(PRODUCT_OUT)/kernel
-	@echo Building additional kernel module $*
-	$(build_kernel) M=$(abspath $@) modules
-
-# Copy modules in directory pointed by $(KERNEL_MODULES_ROOT)
-# First copy modules keeping directory hierarchy lib/modules/`uname-r`for libkmod
-# Second, create flat hierarchy for insmod linking to previous hierarchy
-$(KERNEL_MODULES_INSTALL): $(PRODUCT_OUT)/kernel $(ALL_EXTRA_MODULES)
-	$(hide) rm -rf $(TARGET_OUT)/lib/modules
-	$(build_kernel) modules_install
-	$(hide) for kmod in "$(TARGET_EXTRA_KERNEL_MODULES)" ; do \
-		echo Installing additional kernel module $${kmod} ; \
-		$(subst +,,$(subst $(hide),,$(build_kernel))) M=$(abspath $(TARGET_OUT_INTERMEDIATES))/$${kmod}.kmodule modules_install ; \
-	done
-	$(hide) rm -f $(TARGET_OUT)/lib/modules/*/{build,source}
-{{#x86_64}}
-	$(hide) rm -rf $(TARGET_OUT)/lib64/modules
-	$(hide) cp -rf $(TARGET_OUT)/lib/modules/$(KERNELRELEASE)/ $(TARGET_OUT)/lib64/modules
-	$(hide) ln -s /lib64/modules/$$(basename $$f) $(TARGET_OUT)/lib/modules/$(KERNELRELEASE)/$$f || exit 1;
-{{/x86_64}}
-{{^x86_64}}
-		$(hide) cp -rf $(TARGET_OUT)/lib/modules/$(KERNELRELEASE)/ $(TARGET_OUT)/lib/modules
-		$(hide) ln -s /lib/modules/$$(basename $$f) $(TARGET_OUT)/lib/modules/$(KERNELRELEASE)/$$f || exit 1;
-{{/x86_64}}
-	$(hide) touch $@
-
-# Makes sure any built modules will be included in the system image build.
-ALL_DEFAULT_INSTALLED_MODULES += $(KERNEL_MODULES_INSTALL)
 
 installclean: FILES += $(KERNEL_OUT) $(PRODUCT_OUT)/kernel
 
